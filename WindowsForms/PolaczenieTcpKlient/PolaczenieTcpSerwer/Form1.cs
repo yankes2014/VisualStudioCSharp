04c5de71f3879c03dd3427613d8121c28e932427
yankes2014 <dominik-goldyn1@wp.pl>
2018-01-04 20:18:30 +0100

    Podmianki

--- WindowsForms/PolaczenieTcpKlient/PolaczenieTcpSerwer/Form1.cs
+++ WindowsForms/PolaczenieTcpKlient/PolaczenieTcpSerwer/Form1.cs
@@ -28,20 +28,44 @@ namespace PolaczenieTcpSerwer
         private void button1_Click(object sender, EventArgs e)
         {
             IPAddress adresIP = null;
-            try { adresIP = IPAddress.Parse(textBox1.Text); }
-            catch { MessageBox.Show("Błędny format adresu IP!", "Błąd"); textBox1.Text = String.Empty; return; }
+            try 
+            { 
+                adresIP = IPAddress.Parse(textBox1.Text); 
+            }
+            catch 
+            { 
+                MessageBox.Show("Błędny format adresu IP!", "Błąd"); 
+                textBox1.Text = String.Empty; 
+                return; 
+            }
             int port = System.Convert.ToInt16(numericUpDown1.Value);
-            try { serwer = new TcpListener(adresIP, port); serwer.Start(); klient = serwer.AcceptTcpClient(); listBox1.Items.Add("Nawiązano połączenie"); button1.Enabled = false; button2.Enabled = true;
-            IPEndPoint IP = (IPEndPoint)klient.Client.RemoteEndPoint; listBox1.Items.Add("[" + IP.ToString() + "] :Nawiązano połączenie"); 
+            try 
+            { 
+                serwer = new TcpListener(adresIP, port); 
+                serwer.Start(); klient = serwer.AcceptTcpClient(); 
+                listBox1.Items.Add("Nawiązano połączenie"); 
+                button1.Enabled = false; 
+                button2.Enabled = true;
+                IPEndPoint IP = (IPEndPoint)klient.Client.RemoteEndPoint; 
+                listBox1.Items.Add("[" + IP.ToString() + "] :Nawiązano połączenie"); 
                 klient.Close(); serwer.Stop();
             }
-            catch (Exception ex) { listBox1.Items.Add("Błąd inicjacji serwera!"); MessageBox.Show(ex.ToString(), "Błąd"); }
+            catch (Exception ex) 
+            { 
+                listBox1.Items.Add("Błąd inicjacji serwera!"); 
+                MessageBox.Show(ex.ToString(), "Błąd"); 
+            }
+                
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            serwer.Stop(); klient.Close(); listBox1.Items.Add("Zakończono pracę serwera ..."); button1.Enabled = true; button2.Enabled = false; 
+            serwer.Stop(); 
+            klient.Close(); 
+            listBox1.Items.Add("Zakończono pracę serwera ..."); 
+            button1.Enabled = true; 
+            button2.Enabled = false; 
 
         }
 
